0af52f0e04a31bebc357bc8c7478c36dae86e6cc
yaggod <yaggod@yandex.ru>
2025-01-23 16:56:07 +0300

    finished first subtask

---
+++ task1/Program.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import javax.naming.OperationNotSupportedException;
+
+public class Program
+{
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws OperationNotSupportedException
+	{
+		System.out.println("Enter task id: ");
+		int subTaskId = scanner.nextInt();
+		executeTaskFromId(subTaskId);
+	}
+
+	public static void executeTaskFromId(int id) throws OperationNotSupportedException
+	{
+		switch (id)
+		{
+			case 1:
+				executeSubtask1();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				throw new OperationNotSupportedException("");
+		} 
+	}
+
+    private static void executeSubtask1() 
+	{
+		int stepsCount = 0;
+		long value;
+		System.out.println("Enter base value: ");
+		value = scanner.nextLong();
+		while(value != 1)
+		{
+			if(value % 2 == 0)
+				value /= 2;
+			else
+				value = 3*value + 1;
+			stepsCount++;
+		}
+
+		System.out.println(stepsCount + " steps in total");
+	}
+}
