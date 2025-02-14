a986074d70374eef374d2e73b85348cd6d71a217
yaggod <yaggod@yandex.ru>
2025-02-15 01:24:04 +0300

    finished subtask 6

--- Task2/Program.java
+++ Task2/Program.java
@@ -118,4 +118,14 @@ public class Program
                     return new int[]{array[i], array[j]};
         return null;
     }
+
+    private static int GetTotalSum(int[][] matrix)
+    {
+        int result = 0;
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = 0; j < matrix[i].length; j++)
+                result += matrix[i][j];
+
+        return result;
+    }
 }    
