bfc941396ba8bc02d44924cb049b5f5ca3a1517a
yaggod <yaggod@yandex.ru>
2025-03-15 07:41:55 +0300

    Changed some cinema class implementation details

--- Task3/ticketsystem/Cinema.java
+++ Task3/ticketsystem/Cinema.java
@@ -3,10 +3,37 @@ import java.util.*;
 
 public class Cinema
 {
-    ArrayList<CinemaRoom> allRooms = new ArrayList<>();
+    private static ArrayList<Cinema> cinemas = new ArrayList<>();
+
+    private ArrayList<CinemaRoom> allRooms = new ArrayList<>();
+    private String cinemaName;
+
+    public Cinema(String name)
+    {
+        cinemaName = name;
+    }
+
+    
+
+
+    public static ArrayList<Cinema> GetCinemas() 
+    {
+        return cinemas;
+    }
+
+    public static void AddCinema(Cinema cinema) 
+    {
+        cinemas.add(cinema);
+    }
+
 
     public void AddRoom(CinemaRoom room)
     {
         allRooms.add(room);
+    }
+
+    public String GetCinemaName() 
+    {
+        return cinemaName;
     }
 }
